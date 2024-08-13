8169e9ac9e55a4285ca45bd80b009fe2e827cce6
Mihail Santean <128645014+MSantean@users.noreply.github.com>
2024-08-13 01:41:52 -0400

    Update Multiples of 3 or 5.cpp

--- Multiples of 3 or 5.cpp
+++ Multiples of 3 or 5.cpp
@@ -1,16 +1,19 @@
+// https://projecteuler.net/problem=1
+
 #include <iostream>
 
 int main()
 {
 	int sum = 0;
+	int n = 1000;
 
-	for (int i = 3; i < 1000; ++i)
+	for (int i = 3; i < n; ++i)
 	{
 		if (i % 3 == 0 || i % 5 == 0)
 			sum += i;
 	}
 
-	std::cout << "sum of multiples of 3 or 5 below 1000 is " << sum << '\n';
+	std::cout << "sum of multiples of 3 or 5 below " << n << " is " << sum << '\n';
 
 	return 0;
 }
